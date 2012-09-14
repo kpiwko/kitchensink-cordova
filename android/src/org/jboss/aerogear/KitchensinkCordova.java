4e187b673d3cb76b025afb47cc452d09ee813862
Kris Borchers <kris.borchers@gmail.com>
2012-09-14 11:34:47 -0500

    Customize User-Agent string and then detect that custom string to dynamically load the correct cordova.js file

--- android/src/org/jboss/aerogear/KitchensinkCordova.java
+++ android/src/org/jboss/aerogear/KitchensinkCordova.java
@@ -3,6 +3,7 @@ package org.jboss.aerogear;
 import android.app.Activity;
 import android.os.Bundle;
 import org.apache.cordova.*;
+import android.webkit.WebSettings;
 
 public class KitchensinkCordova extends DroidGap
 {
@@ -11,6 +12,14 @@ public class KitchensinkCordova extends DroidGap
     {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+    }
+    
+    @Override
+    public void init() {
+    	super.init();
+    	
+    	WebSettings settings = this.appView.getSettings();
+    	settings.setUserAgentString("Kitchensink Cordova Webview Android");
     }
 }
 
